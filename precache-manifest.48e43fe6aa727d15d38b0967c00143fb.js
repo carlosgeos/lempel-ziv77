6943f77502ce02769c321f2e65814db5fd5c10cf
Carlos Requena López <crequena@ulb.ac.be>
2019-09-20 17:01:50 +0200

    Updates

--- precache-manifest.48e43fe6aa727d15d38b0967c00143fb.js
+++ precache-manifest.48e43fe6aa727d15d38b0967c00143fb.js
@@ -1,23 +1,27 @@
-self.__precacheManifest = [
+self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c87e16e9cc0d56d4e418",
-    "url": "/lempel-ziv77/static/css/main.5efb2901.chunk.css"
+    "revision": "f37947da6c3040756652a1fc58c5b19e",
+    "url": "/lempel-ziv77/index.html"
   },
   {
-    "revision": "c87e16e9cc0d56d4e418",
-    "url": "/lempel-ziv77/static/js/main.c87e16e9.chunk.js"
+    "revision": "b5de6c380c372b439f36",
+    "url": "/lempel-ziv77/static/css/2.9acac71f.chunk.css"
   },
   {
-    "revision": "99b219ee547430f9b01a",
-    "url": "/lempel-ziv77/static/css/1.efe86363.chunk.css"
+    "revision": "c3f14ed514c83e993074",
+    "url": "/lempel-ziv77/static/css/main.96043103.chunk.css"
   },
   {
-    "revision": "99b219ee547430f9b01a",
-    "url": "/lempel-ziv77/static/js/1.99b219ee.chunk.js"
+    "revision": "b5de6c380c372b439f36",
+    "url": "/lempel-ziv77/static/js/2.882cf786.chunk.js"
   },
   {
-    "revision": "fed72473103343371cbd",
-    "url": "/lempel-ziv77/static/js/runtime~main.fed72473.js"
+    "revision": "c3f14ed514c83e993074",
+    "url": "/lempel-ziv77/static/js/main.b56eb6dd.chunk.js"
+  },
+  {
+    "revision": "00043f9e0d6e983398ca",
+    "url": "/lempel-ziv77/static/js/runtime-main.6507f5cd.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
@@ -28,60 +32,52 @@ self.__precacheManifest = [
     "url": "/lempel-ziv77/static/media/roboto-latin-100.987b8457.woff2"
   },
   {
-    "revision": "55536c8e9e9a532651e3cf374f290ea3",
-    "url": "/lempel-ziv77/static/media/roboto-latin-300.55536c8e.woff2"
-  },
-  {
-    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
-    "url": "/lempel-ziv77/static/media/roboto-latin-400.5d4aeb4e.woff2"
-  },
-  {
-    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
-    "url": "/lempel-ziv77/static/media/roboto-latin-500.28546717.woff2"
-  },
-  {
-    "revision": "037d830416495def72b7881024c14b7b",
-    "url": "/lempel-ziv77/static/media/roboto-latin-700.037d8304.woff2"
-  },
-  {
-    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
-    "url": "/lempel-ziv77/static/media/roboto-latin-900.19b7a0ad.woff2"
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/lempel-ziv77/static/media/roboto-latin-100.e9dbbe8a.woff"
   },
   {
     "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
     "url": "/lempel-ziv77/static/media/roboto-latin-100italic.6232f43d.woff2"
   },
   {
-    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
-    "url": "/lempel-ziv77/static/media/roboto-latin-100.e9dbbe8a.woff"
-  },
-  {
     "revision": "d704bb3d579b7d5e40880c75705c8a71",
     "url": "/lempel-ziv77/static/media/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/lempel-ziv77/static/media/roboto-latin-300.55536c8e.woff2"
   },
   {
     "revision": "a1471d1d6431c893582a5f6a250db3f9",
     "url": "/lempel-ziv77/static/media/roboto-latin-300.a1471d1d.woff"
   },
   {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/lempel-ziv77/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
     "revision": "d69924b98acd849cdeba9fbff3f88ea6",
     "url": "/lempel-ziv77/static/media/roboto-latin-300italic.d69924b9.woff2"
   },
   {
-    "revision": "210a7c781f5a354a0e4985656ab456d9",
-    "url": "/lempel-ziv77/static/media/roboto-latin-300italic.210a7c78.woff"
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/lempel-ziv77/static/media/roboto-latin-400.5d4aeb4e.woff2"
   },
   {
     "revision": "bafb105baeb22d965c70fe52ba6b49d9",
     "url": "/lempel-ziv77/static/media/roboto-latin-400.bafb105b.woff"
   },
   {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/lempel-ziv77/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
     "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
     "url": "/lempel-ziv77/static/media/roboto-latin-400italic.d8bcbe72.woff2"
   },
   {
-    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
-    "url": "/lempel-ziv77/static/media/roboto-latin-400italic.9680d5a0.woff"
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/lempel-ziv77/static/media/roboto-latin-500.28546717.woff2"
   },
   {
     "revision": "de8b7431b74642e830af4d4f4b513ec9",
@@ -96,6 +92,10 @@ self.__precacheManifest = [
     "url": "/lempel-ziv77/static/media/roboto-latin-500italic.ffcc050b.woff"
   },
   {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/lempel-ziv77/static/media/roboto-latin-700.037d8304.woff2"
+  },
+  {
     "revision": "cf6613d1adf490972c557a8e318e0868",
     "url": "/lempel-ziv77/static/media/roboto-latin-700.cf6613d1.woff"
   },
@@ -108,6 +108,10 @@ self.__precacheManifest = [
     "url": "/lempel-ziv77/static/media/roboto-latin-700italic.846d1890.woff"
   },
   {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/lempel-ziv77/static/media/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
     "revision": "8c2ade503b34e31430d6c98aa29a52a3",
     "url": "/lempel-ziv77/static/media/roboto-latin-900.8c2ade50.woff"
   },
@@ -118,9 +122,5 @@ self.__precacheManifest = [
   {
     "revision": "bc833e725c137257c2c42a789845d82f",
     "url": "/lempel-ziv77/static/media/roboto-latin-900italic.bc833e72.woff"
-  },
-  {
-    "revision": "8d87763aa98e01b3174883b237c38337",
-    "url": "/lempel-ziv77/index.html"
   }
-];
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lempel-ziv77/precache-manifest.861eb9973ac378f7a48769d6c7d7b572.js"
+  "/lempel-ziv77/precache-manifest.48e43fe6aa727d15d38b0967c00143fb.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/lempel-ziv77/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lempel-ziv77/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
